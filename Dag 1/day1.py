472cd9e287d2deb40291ced21c2c44c148e2bd63
Björn Mårtensson <bearmarshal@gmail.com>
2020-12-05 20:01:35 +0100

    Dag 1 del 2

--- Dag 1/day1.py
+++ Dag 1/day1.py
@@ -53,6 +53,7 @@ def first(file_name):
 	small = numbers[index_s]
 	large = numbers[index_l]
 	index_s_raised = False # Whenever we lower index_l, we need to backtrack on index_s to make sure we didn't overstep the solution
+	# Time complexity: N
 	while small + large != target:
 		if small + large < target:
 			index_s += 1
@@ -65,6 +66,61 @@ def first(file_name):
 		small = numbers[index_s]
 		large = numbers[index_l]
 	print("First star: {}".format(small * large))
+
+def second(file_name):
+	search_tree = SearchTree()
+	with io.open(file_name, 'r') as infile:
+		for line in infile:
+			search_tree.add(int(line))
+	numbers = list(search_tree) # Simplify iteration
+	target = 2020
+	index_s = 0
+	index_m = 1
+	index_l = len(numbers) - 1
+	small = numbers[index_s]
+	medium = numbers[index_m]
+	large = numbers[index_l]
+	"""
+	Raise index_m to the first value for this index_s and index_l which produces a too large result.
+	Run the algorithm in the first solution on index_s and index_m.
+	If they meet, reset index_s to 0 and index_m to index which produced the too large result, lower index_l and start over.
+	
+	Time complexity N^2
+	"""
+	last_too_large_index_m = 0
+	index_s_raised = False
+	while small + medium + large != target:
+		if small + medium + large < target:
+			if index_m + 1 < index_l:
+				index_m += 1
+			else:
+				index_s += 1
+				small = numbers[index_s]
+				while small + medium + large > target and index_m - 1 > index_s:
+					index_m -= 1
+					medium = numbers[index_m]
+		else:
+			last_too_large_index_m = index_m
+			while small + medium + large != target:
+				if small + medium + large < target:
+					index_s += 1
+				else:
+					index_m -= 1
+					medium = numbers[index_m]
+					while small + medium + large > target and index_s > 0:
+						index_s -= 1
+						small = numbers[-1]
+				if index_s == index_m:
+					index_s = 0
+					index_m = last_too_large_index_m
+					index_l -= 1
+					break
+				small = numbers[index_s]	
+		small = numbers[index_s]
+		medium = numbers[index_m]
+		large = numbers[index_l]
+	print("Second star: {}".format(small * medium * large))
 			
 if (__name__ == '__main__'):
 	first(sys.argv[1])
+	second(sys.argv[1])

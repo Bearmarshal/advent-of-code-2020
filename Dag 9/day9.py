825c6a601aca5239e69e69107c10212fec924509
Björn Mårtensson <bearmarshal@gmail.com>
2020-12-09 08:53:13 +0100

    Dag 9

--- Dag 9/day9.py
+++ Dag 9/day9.py
@@ -6,32 +6,66 @@ import re
 import sys
 
 def first(file_name):
-    previous = []
+    previous = collections.deque()
     matching_numbers = {}
-    i = 0
     with io.open(file_name, mode = 'r') as infile:
         for line in infile:
-            number = int(infile.readline())
+            number = int(line)
+            if len(previous) == 25:
+                if number not in matching_numbers:
+                    break
+                else:
+                    oldest = previous.popleft()
+                    for other_number in previous:
+                        sum = oldest + other_number
+                        if matching_numbers[sum] == 1:
+                            del(matching_numbers[sum])
+                        else:
+                            matching_numbers[sum] -= 1
             for other_number in previous:
                 sum = number + other_number
                 if sum not in matching_numbers:
                     matching_numbers[sum] = 0
                 matching_numbers[sum] += 1
-            previous += [number]
-        matching_numbers = {a + b: (a, b) for a in previous for b in previous if a != b}
-        for line in infile:
-            number = int(line)
-            if number not in matching_numbers:
-                print("First star: {}".format(number))
-                break
-            else:
-                print("{} = {} + {}".format(number, matching_numbers[number][0], matching_numbers[number][1]))
+            previous.append(number)
+    print("First star: {}".format(number))
 
 def second(file_name):
     with io.open(file_name, mode = 'r') as infile:
-        indata = infile.read()
-    print("Second star: {}".format(acc))
+        indata = [int(line) for line in infile]
+    previous = collections.deque()
+    matching_numbers = {}
+    for number in indata:
+        if len(previous) == 25:
+            if number not in matching_numbers:
+                break
+            else:
+                oldest = previous.popleft()
+                for other_number in previous:
+                    sum = oldest + other_number
+                    if matching_numbers[sum] == 1:
+                        del(matching_numbers[sum])
+                    else:
+                        matching_numbers[sum] -= 1
+        for other_number in previous:
+            sum = number + other_number
+            if sum not in matching_numbers:
+                matching_numbers[sum] = 0
+            matching_numbers[sum] += 1
+        previous.append(number)
+    start_i = 0
+    end_i = -1
+    sum = 0
+    while sum != number:
+        if sum < number:
+            end_i += 1
+            sum += indata[end_i]
+        else:
+            sum -= indata[start_i]
+            start_i += 1
+    else:
+        print("Second star: {}".format(indata[start_i] + indata[end_i]))
 
 if __name__ == "__main__":
     first(sys.argv[1])
-    #second(sys.argv[1])
+    second(sys.argv[1])

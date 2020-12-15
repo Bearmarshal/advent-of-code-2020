dc949e280a60b2053e8d6b0a8724e6db9bc91d8e
3jörn Mårtensson <bearmarshal@gmail.com>
2020-12-15 07:21:57 +0100

    Dag 15, del 2, brute force

--- Dag 15/day15.py
+++ Dag 15/day15.py
@@ -25,11 +25,24 @@ def first(file_name):
         turn += 1
     print("First star: {}".format(curr_number))
 
-# def second(file_name):
-#     with io.open(file_name, mode = 'r') as infile:
-#         indata = infile.read()
-#     print("Second star: {}".format(0))
+def second(file_name):
+    with io.open(file_name, mode = 'r') as infile:
+        indata = infile.read()
+    curr_number = 0
+    turn = 0
+    memory = {(curr_number := number): ((turn := i), 0) for i, number in enumerate(map(int, re.findall(r'\d+', indata)), 1)}
+    turn += 1
+    while turn <= 30000000:
+        when_last_said, when_said_before = memory[curr_number]
+        if not when_said_before:
+            curr_number = 0
+        else:
+            curr_number = when_last_said - when_said_before
+        curr_last_said, _ = memory.get(curr_number, (0, 0))
+        memory[curr_number] = turn, curr_last_said
+        turn += 1
+    print("Second star: {}".format(curr_number))
 
 if __name__ == "__main__":
     first(sys.argv[1])
-    # second(sys.argv[1])
+    second(sys.argv[1])
